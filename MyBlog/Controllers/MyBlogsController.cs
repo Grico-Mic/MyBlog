0e839b6f1268c6ba030473972df00d8e7734e248
Grico-Mic <igorgric@outlook.com>
2023-05-03 18:35:12 +0200

    Added layout, details and info pages.

--- MyBlog/Controllers/MyBlogsController.cs
+++ MyBlog/Controllers/MyBlogsController.cs
@@ -9,7 +9,7 @@ namespace MyBlog.Controllers
 {
     public class MyBlogsController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Overview()
         {
             var myBlog1 = new Blog();
             myBlog1.Id = 1;
@@ -18,9 +18,9 @@ namespace MyBlog.Controllers
             myBlog1.DateCreated = DateTime.Now;
 
             var myBlog2 = new Blog();
-            myBlog2.Id = 1;
+            myBlog2.Id = 2;
             myBlog2.Title = "Hello from the other side";
-            myBlog2.Description = "This is some description number 2";
+            myBlog2.Description = "This is some description number 2.";
             myBlog2.DateCreated = DateTime.Now;
 
             var myBlogs = new List<Blog> { myBlog1, myBlog2 };
