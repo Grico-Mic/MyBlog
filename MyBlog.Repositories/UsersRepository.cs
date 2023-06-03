f5e034095ad2bcb89318914fccca94457d8b974c
Grico-Mic <igorgric@outlook.com>
2023-06-03 17:00:38 +0200

    Create repository for model User.

---
+++ MyBlog.Repositories/UsersRepository.cs
@@ -0,0 +1,23 @@
+﻿using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyBlog.Repositories
+{
+    public class UsersRepository
+    {
+        private MyBlogDbContext _context;
+
+        public UsersRepository(MyBlogDbContext context)
+        {
+            this._context = context;
+        }
+
+        public List<User> GetAll ()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
